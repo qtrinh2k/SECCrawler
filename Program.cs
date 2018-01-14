b70a33fdbdb3543a2bae74ce8b3e3a8b66127c8f
unknown <qtrinh2k@gmail.com>
2018-01-14 00:10:43 -0800

    day 2: get filling report from rss feed

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.IO;
 using System.Text;
 using System.Net;
+using System.Xml;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace SECCrawler
 {
@@ -11,30 +14,60 @@ namespace SECCrawler
     {
         static void Main(string[] args)
         {
-            string cikTickerPath = @"C:\Users\Matrix-PC\source\repos\SimpleAngular\SECCrawler\cik_ticker.csv";
+            string cikTickerPath = @"C:\Users\Matrix-PC\source\repos\SECCrawler\cik_ticker.csv";
 
             var cikInfos = ImportCIKInfo(cikTickerPath);
 
             foreach(var cik in cikInfos)
             {
                 Console.WriteLine("Downloading Ticker={0}, CIK={1}", cik.Ticker, cik.CIK);
-                GetSECFilling(cik, ReportType.Annual);
+                string downloadFile = GetSECFilling(cik, ReportType.Annual);
+
+                //down each filling report
+                //entry/content/filing-href
+                //XmlDocument xdoc = new XmlDocument();
+                //xdoc.Load(downloadFile);
+
+                //XmlNamespaceManager mgr = new XmlNamespaceManager(xdoc.NameTable);
+                //mgr.AddNamespace("x", "http://schemas.openxmlformats.org/officeDocument/2006/extended-properties");
+
+                //XmlNodeList nodes = xdoc.SelectNodes("//feed");
+                XElement xml = XElement.Load(downloadFile);
+                string fillingHref = xml.Descendants("filling-href").Select(x => x.Value).FirstOrDefault();
+                Console.WriteLine(fillingHref);
+                //foreach (XmlNode node in nodes)
+                //{
+                //    string content = DownloadContent(node.InnerText);
+
+                //    search content from report
+                //    Archives(.)*10k\.htm
+                //    Regex regex = new Regex("Archives(.)*10k\\.htm", RegexOptions.Multiline);
+                //    Match result = regex.Match(content);
+
+                //    Console.WriteLine(result.Value);
+                //}
             }
 
         }
 
-        private static void GetSECFilling(CIKInfo cikInfo, ReportType reportType)
+        private static string GetSECFilling(CIKInfo cikInfo, ReportType reportType)
         {
 
             //string url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={cikInfo.CIK}&type=10-K&dateb=&owner=include&count=10";
             string rssUrl = string.Format("https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={0}&type=10-K%25&dateb=&owner=include&start=0&count=10&output=atom", cikInfo.CIK);
+
+            string path = string.Format("c:\\temp\\SEC\\{0}_{1}", cikInfo.Ticker, cikInfo.CIK);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            string summaryFile = string.Format("{0}\\{1}_{2}.txt", path, cikInfo.Ticker, cikInfo.CIK);
 
             using (WebClient client = new WebClient())
             {
                 try
                 {
                     Console.WriteLine(rssUrl);
-                    client.DownloadFile(rssUrl, string.Format("c:\\temp\\SEC\\{0}_{1}.txt", cikInfo.Ticker, cikInfo.CIK));
+                    client.DownloadFile(rssUrl, summaryFile);
                 }
                 catch(Exception ex)
                 {
@@ -42,6 +75,26 @@ namespace SECCrawler
                     Console.WriteLine(ex.Message);
                 }
             }
+
+            return summaryFile;
+        }
+
+        private static string DownloadContent(string url)
+        {
+            Console.WriteLine(url);
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    return client.DownloadString(url);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return null;
         }
 
         public static IEnumerable<CIKInfo> ImportCIKInfo(string cikTickerPath)
