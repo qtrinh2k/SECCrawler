ceeb0850581ec5fb649d188b7b5a6b64fa70915d
Quoc Trinh <qtrinh2k@gmail.com>
2018-01-28 08:11:35 -0800

    testing

--- WebAppTests/CompanyControllerTests.cs
+++ WebAppTests/CompanyControllerTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using WebApp.Controllers;
+using SECReportWeb.Controllers;
 
 namespace WebAppTests
 {
@@ -10,12 +11,22 @@ namespace WebAppTests
         public void Test1()
         {
             CompanyController controller = new CompanyController();
-            //var results = controller.GetCompanyInfo();
-            //Assert.NotNull(results);
-            //var tickerInfo = controller.GetByCIK("1090872");
-            //Assert.NotNull(tickerInfo);
 
-            var result = controller.GetCompanyInfoByTicker("AA");
+            var results = controller.GetCompanyInfo();
+            Assert.NotNull(results);
+
+            var tickerInfo = controller.GetByCIK("1090872");
+            Assert.NotNull(tickerInfo);
+
+            var result = controller.GetByTicker("AA");
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            SECReportController controller = new SECReportController();
+            var result = controller.GetCompanyInfo();
             Assert.NotNull(result);
         }
     }
